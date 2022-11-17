247000720c7de37a2e94f9d9b1b02dafe9502e32
Nabil Shahid <nabilmd901@gmail.com>
2022-11-17 23:33:52 +0600

    routing done

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+
 import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory() || createMemoryHistory();
+  if (onNavigate) history.listen(onNavigate);
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) history.push(nextPathname);
+    },
+  };
 };
 
 //Standalone
 if (process.env.NODE_ENV === "development") {
   const root = document.querySelector("#__marketing-root__");
-  if (root) mount(root);
+  if (root) mount(root, { defaultHistory: createBrowserHistory });
 }
 
 //With Container
