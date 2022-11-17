20977f8eddc0df481882fd9cec321adbeb1980c1
Nabil Shahid <nabilmd901@gmail.com>
2022-11-18 01:35:42 +0600

    auth done

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -6,6 +6,7 @@ import {
 } from "@material-ui/core/styles";
 import Header from "./components/Header";
 import Progress from "./components/Progress";
+import { useState } from "react";
 
 const AuthApp = lazy(() => import("./components/AuthApp"));
 const MarketingApp = lazy(() => import("./components/MarketingApp"));
@@ -13,14 +14,20 @@ const MarketingApp = lazy(() => import("./components/MarketingApp"));
 const generateClassName = createGenerateClassName({ productionPrefix: "co" });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <div>
-          <Header />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
           <Suspense fallback={<Progress />}>
             <Switch>
-              <Route path="/auth" component={AuthApp} />
+              <Route path="/auth">
+                <AuthApp onSignIn={() => setIsSignedIn(true)} />
+              </Route>
               <Route path="/" component={MarketingApp} />
             </Switch>
           </Suspense>
